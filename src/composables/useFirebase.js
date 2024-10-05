7f6b58f7e13e3ea4b1df1ddfe8117d15008748f6
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-10-05 11:16:13 -0600

    Added main files for website (functional)

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfcHu7GtiWmePG96a6CYYtXwQPVQ-jYD8",
+  authDomain: "project5-a4b37.firebaseapp.com",
+  projectId: "project5-a4b37",
+  storageBucket: "project5-a4b37.appspot.com",
+  messagingSenderId: "524499808079",
+  appId: "1:524499808079:web:25f48f148ff7ced1821f3a"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
