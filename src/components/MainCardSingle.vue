544561fe0f1a9d6c5cd6ea628fa78007e3de18af
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-10-05 17:17:32 -0600

    Updated and stylized cards

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,29 +1,25 @@
 <script setup>
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
 
 const firstname = faker.person.firstName();
 const lastname = faker.person.lastName();
 const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname });
 const job_title = faker.person.jobTitle();
-const bio = faker.lorem.lines(2);
 const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase();
-const phone = faker.phone.imei() 
-
+const phone = faker.phone.imei();
+const avatar = faker.image.avatar(); 
+const bio = "Click here to see more information about this contact"; 
 </script>
 
-
 <template>
-
-<RouterLink :to="{ name: 'CardDetails', params: { id: username, fullname: fullname, job_title: job_title, bio: bio, phone: phone } }">
-<div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
-    <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'cat'})" class="rounded-t-xl" />
-    <div class="p-4">
-        <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+  <RouterLink :to="{ name: 'CardDetails', params: { id: username, fullname, job_title, bio, phone } }">
+    <div class="border border-gray-400 rounded-xl shadow-lg bg-white hover:scale-105 h-full">
+      <img :src="avatar" class="rounded-t-xl w-full h-48 object-cover" />
+      <div class="p-6">
+        <p class="mb-2 text-2xl font-bold text-blue-600">{{ fullname }}</p>
+        <p class="mb-3 font-semibold text-gray-700">{{ job_title }}</p>
+        <p class="mb-3 font-normal text-gray-500">{{ bio }}</p>
+      </div>
     </div>
-</div>
-</RouterLink>
-
-
+  </RouterLink>
 </template>
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -7,8 +7,13 @@ import MainCardSingle from './MainCardSingle.vue';
 </script>
 
 <template>
-<div class="grid grid-cols-5 gap-10 p-10">
-    <MainCardSingle v-for = "card in 20" :key="card"> </MainCardSingle>
+<!-- Header -->
+<div class="text-center bg-blue-600 text-white py-4">
+    <h1 class="text-4xl font-bold">Contact List</h1>
+</div>
+
+<div class="grid grid-cols-3 gap-5 p-6 bg-gray-100 min-h-screen">
+    <MainCardSingle v-for = "card in 12" :key="card"> </MainCardSingle>
 </div>
 
 
