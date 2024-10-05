75498d9c3a6e0da124377080627dc51c64fe8aeb
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-10-05 16:01:10 -0600

    stylized card details

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -12,11 +12,18 @@ const phone = route.params.phone;
 </script>
 
 <template>
-  <div>
-    <h1>{{ fullname }}</h1>
-    <p>Username: {{ username }}</p>
-    <p>Job Title: {{ job_title }}</p>
-    <p>Bio: {{ bio }}</p>
-    <p>Phone: {{ phone }}</p>
+  <div class="p-6 bg-white shadow-lg rounded-lg max-w-md mx-auto"> 
+    <h2 class="text-3xl font-semibold text-gray-800">{{ name }}</h2> 
+    <p class="text-lg text-gray-600 font-bold">Username:</p> 
+    <p class="text-lg text-gray-800">{{ username }}</p> 
+    
+    <p class="text-lg text-gray-600 font-bold">Job Title:</p> 
+    <p class="text-lg text-gray-800">{{ job_title }}</p> 
+    
+    <p class="text-lg text-gray-600 font-bold">Bio:</p> 
+    <p class="text-lg text-gray-800">{{ bio }}</p> 
+    
+    <p class="text-lg text-gray-600 font-bold">Phone:</p> 
+    <p class="text-lg text-gray-800">{{ phone }}</p> 
   </div>
 </template>
