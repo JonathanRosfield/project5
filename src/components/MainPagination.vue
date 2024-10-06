92b6bfc0ca58ceadcd7fda127c890bb1253feab2
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-10-05 22:56:05 -0600

    Fixed pagination styling

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -2,7 +2,7 @@
 <div class="flex justify-center gap-4 p-8">
 
     <button @click="prevPage" :disabled="activePage === 1" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-300 disabled:opacity-50">PREV</button>
-    <button v-for="page in pages" :key="page" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md" :class="page === activePage ? 'bg-blue-500 text-slate-100': 'bg-slate-100 hover:bg-slate-300'">{{ page }}</button>
+    <button v-for="page in pages" :key="page" :class="[ 'rounded-md p-2 font-medium shadow-md', page === activePage ? 'bg-blue-500 text-slate-100' : 'bg-slate-100 text-slate-700 hover:bg-slate-300']">{{ page }}</button>
     <button @click="nextPage" :disabled="activePage === pages" class="rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-300 disabled:opacity-50">NEXT</button>
 
 
